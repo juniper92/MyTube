9ed9d69698d5a69ca13b2b8b95cce4cfa83c32e1
juniper <heyjuniper92@gmail.com>
2022-04-21 16:16:37 +0900

    Initial Commit

---
+++ MyTube/MyTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTubeApp.swift
+//  MyTube
+//
+//  Created by Mila on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MyTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
